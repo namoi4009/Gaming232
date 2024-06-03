3f38fb0d4d2f5d2b7d53260e6225c031c36c54e3
CUCCUTTHANBI\tango <tangocnam23@gmail.com>
2024-06-03 11:17:38 +0700

    All project

---
+++ Assets/Scripts/Car/CarHandler.cs
@@ -0,0 +1,118 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class CarHandler : MonoBehaviour
+{
+    [SerializeField]
+    Rigidbody rb;
+
+    [SerializeField]
+    Transform gameModel;
+
+    // Max values
+    float maxSteerVelocity = 2;
+    float maxFowardVelocity = 30;
+
+    // Multiplier
+    float accelerateMultiplier = 3f;
+    float brakeMultiplier = 15f;
+    float steeringMultiplier = 5f;
+
+    // Input
+    Vector2 input = Vector2.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (rb == null)
+        {
+            rb = GetComponent<Rigidbody>();
+        }
+    }
+
+    // Update is called once per frames
+    void Update()
+    {
+        // Rotate car when turning
+        gameModel.transform.rotation = Quaternion.Euler(0, rb.velocity.x * 5, 0);
+    }
+
+    private void FixedUpdate()
+    {
+        // Execute Acceleration
+        if (input.y > 0)
+            Accelerate();
+        else
+            rb.drag = 0.2f;
+
+        // Execute Brake
+        if (input.y < 0)
+            Brake();
+
+        // Execute left and right moving
+        Steer();
+
+        // Avoid moving backward
+        if (rb.velocity.z <= 0)
+            rb.velocity = Vector3.zero;
+    }
+
+    void Accelerate()
+    {
+        rb.drag = 0;
+
+        // If the velocity is too fast, stop accelerating
+        if (rb.velocity.z >= maxFowardVelocity)
+            return;
+
+        // Accelerating
+        rb.AddForce(rb.transform.forward * accelerateMultiplier * input.y);
+    }
+
+    void Brake()
+    {
+        // Don't brake if the car stand still
+        if (rb.velocity.z <= 0)
+            return;
+
+        rb.AddForce(rb.transform.forward * brakeMultiplier * input.y);
+    }
+
+    void Steer()
+    {
+        if (Mathf.Abs(input.x) > 0)
+        {
+            // If we go slow, we steer slow
+            float speedBaseSteerLimit = rb.velocity.z / 0.5f;
+            speedBaseSteerLimit = Mathf.Clamp01(speedBaseSteerLimit);
+            
+            // steering
+            rb.AddForce(rb.transform.right * steeringMultiplier * input.x * speedBaseSteerLimit);
+
+            // Normalize the X Velocity
+            float normalizedX = rb.velocity.x / maxSteerVelocity;
+
+            // Ensure magnitude <= 1
+            normalizedX = Mathf.Clamp(normalizedX, -1.0f, 1.0f);
+
+            // Make sure out steering speed is under the limit
+            rb.velocity = new Vector3(normalizedX * maxSteerVelocity, 0, rb.velocity.z);
+        }
+        else
+        {
+            // Auto center car
+            rb.velocity = Vector3.Lerp(rb.velocity, new Vector3(0, 0 ,rb.velocity.z), Time.fixedDeltaTime * 3);
+
+        }
+    }
+
+    public void setInput(Vector2 inputVector)
+    {
+        inputVector.Normalize();
+
+        input = inputVector;
+    }
+}
---
+++ Assets/Scripts/Endless/EndlessLevelHandler.cs
@@ -0,0 +1,109 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndlessLevelHandler : MonoBehaviour
+{
+    [SerializeField]
+    GameObject[] sectionsPrefabs;
+
+    GameObject[] sectionsPool = new GameObject[30];
+
+    // Choose from Section Pool to appear on game
+    GameObject[] sections = new GameObject[10];
+
+    Transform playerCarTransform;
+
+    WaitForSeconds waitFor100ms = new WaitForSeconds(0.1f);
+
+    const float sectionLength = 24;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerCarTransform = GameObject.FindGameObjectWithTag("Player").transform;
+
+        int prefabIndex = 0;
+
+        // Create prefab pool
+        for (int i = 0; i < sectionsPool.Length; i++)
+        {
+            sectionsPool[i] = Instantiate(sectionsPrefabs[prefabIndex]);
+            sectionsPool[i].SetActive(false);
+
+            prefabIndex++;
+
+            // Reset prefabIndex if it exceeds the limit
+            if (prefabIndex > sectionsPrefabs.Length - 1)
+                prefabIndex = 0;
+        }
+
+        for (int i = 0; i < sections.Length; i++)
+        {
+            // Get a random section
+            GameObject randomSection = GetRandomSectionFromPool();
+
+            // Move the section into position and set ACTIVE
+            randomSection.transform.position = new Vector3(sectionsPool[i].transform.position.x, -10, i * sectionLength);
+            randomSection.SetActive(true);
+
+            // Set the section chosen into the array
+            sections[i] = randomSection;
+        }
+
+        StartCoroutine(UpdateLessOftenCO());
+
+    }
+
+    IEnumerator UpdateLessOftenCO()
+    {
+        while (true)
+        {
+            UpdateSectionPosition();
+            yield return waitFor100ms;
+        }
+    }
+
+    void UpdateSectionPosition()
+    {
+        for (int i = 0; i < sections.Length; i++)
+        {
+            // Check if the section is too far behind
+            if (sections[i].transform.position.z - playerCarTransform.position.z < -sectionLength)
+            {
+                // Store the position and disable
+                Vector3 lastSectionPosition = sections[i].transform.position;
+                sections[i].SetActive(false);
+
+                // Get new section
+                sections[i] = GetRandomSectionFromPool();
+
+                // Move the new section and activate it
+                sections[i].transform.position = new Vector3(lastSectionPosition.x, -10, lastSectionPosition.z + sectionLength * sections.Length);
+                sections[i].SetActive(true);
+            }
+        }
+    }
+
+    GameObject GetRandomSectionFromPool()
+    {
+        // Pick a random index
+        int randomIndex = Random.Range(0, sectionsPool.Length);
+
+        bool isNewSectionFound = false;
+
+        while (!isNewSectionFound)
+        {
+            if (!sectionsPool[randomIndex].activeInHierarchy)
+                isNewSectionFound = true;
+            else
+            {
+                randomIndex++;
+                if (randomIndex > sectionsPool.Length - 1)
+                    randomIndex = 0;
+            }
+        }
+
+        return sectionsPool[randomIndex];
+    }
+}
