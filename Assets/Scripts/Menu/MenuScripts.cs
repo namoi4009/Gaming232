624cd44a112f21c9d34cb7b1f62f970f0fa0bf28
CUCCUTTHANBI\tango <tangocnam23@gmail.com>
2024-06-07 16:42:50 +0700

    Update Quit Button

--- Assets/Scripts/Menu/MenuScripts.cs
+++ Assets/Scripts/Menu/MenuScripts.cs
@@ -9,4 +9,15 @@ public class MenuScripts : MonoBehaviour
     {
         SceneManager.LoadSceneAsync("Stage");
     }
+
+    public void QuitGame()
+    {
+        #if UNITY_STANDALONE
+            Application.Quit();
+        #endif
+
+        #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+    }
 }
